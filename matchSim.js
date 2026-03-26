30c00932a750512db8055d7a4c8b00b09963817c
Abdullah Novruzlu <novruzluabdullah03@gmail.com>
2026-03-26 23:44:04 +0400

    Update Tottenham's generation value from 77 to 81

--- matchSim.js
+++ matchSim.js
@@ -1,6 +1,6 @@
 const premierLeagueTeams = [
     {name: "Liverpool", gen: 92, logo: '<img src="assets/premierTeams/Liverpool_FC.svg.png" alt="Liverpool" />'},
-    {name: "Tottenham", gen: 77, logo: '<img src="assets/premierTeams/Tottenham_Hotspur.svg.png" alt="Tottenham" />'},
+    {name: "Tottenham", gen: 81, logo: '<img src="assets/premierTeams/Tottenham_Hotspur.svg.png" alt="Tottenham" />'},
     {name: "Manchester City", gen: 95 , logo: '<img src="assets/premierTeams/Manchester_City_FC_badge.svg.png" />'},
     {name: "Manchester United", gen: 87 , logo: '<img src="assets/premierTeams/Manusvg.png" />'},
     {name: "Arsenal", gen: 90 , logo: '<img src="assets/premierTeams/Arsenal.png" />'},
